a778854a2eaf5a4a0cdd66cdc4df6fb737d4e369
Abd El-Rahman Adel <abdostarhero@hotmail.com>
2021-12-31 13:55:54 +0200

    write the server code and edit the database.json

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+// import needed dependencies.
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app: express.Application = express();
+const port: number = 8080;
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+app.get('/', (req: Request, res: Response): void => {
+  res.json('hello world');
+});
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`)
+})
