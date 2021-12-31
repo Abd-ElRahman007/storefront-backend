0f6a65c953703cc497f893b87e6c69a36b04a626
Abd El-Rahman Adel <abdostarhero@hotmail.com>
2021-12-31 15:37:05 +0200

    add the enchanted_orders migrations table

--- migrations/sqls/20211231131401-enchanted-orders-down.sql
+++ migrations/sqls/20211231131401-enchanted-orders-down.sql
@@ -1,1 +1,7 @@
-/* Replace with your SQL commands */
+CREATE TABLE
+  enchanted_orders(
+    id serial PRIMARY KEY,
+    quantity INTEGER NOT NULL,
+    enchanted_stuff_id INTEGER NOT NULL REFERENCES enchanted_stuff(id),
+    order_id INTEGER NOT NULL REFERENCES orders(id)
+  );
