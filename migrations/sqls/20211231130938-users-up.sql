32c4ed0d227fccc2a1c1e97161fc5738adb7b46f
Abd El-Rahman Adel <abdostarhero@hotmail.com>
2021-12-31 15:27:05 +0200

    add the users migrations table

--- migrations/sqls/20211231130938-users-up.sql
+++ migrations/sqls/20211231130938-users-up.sql
@@ -1,1 +1,7 @@
-/* Replace with your SQL commands */
+CREATE TABLE
+  users(
+    id serial PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(25) NOT NULL,
+    password VARCHAR(50) NOT NULL
+  );
