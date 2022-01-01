8296543462bedeb8f82282d0c37fae080b5af6c1
Abd-ElRahman007 <abdelrhman123work@gmail.com>
2022-01-01 13:31:58 +0200

    fixed the orders sql

--- migrations/sqls/20211231131405-orders-up.sql
+++ migrations/sqls/20211231131405-orders-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE
   orders(
     id serial PRIMARY KEY,
-    id_product INTEGER REFERENCES enchanted_products(id),
+    id_product INTEGER REFERENCES enchanted_stuff(id),
     quantity INTEGER NOT NULL,
     id_user INTEGER REFERENCES users(id),
     status VARCHAR(10)
