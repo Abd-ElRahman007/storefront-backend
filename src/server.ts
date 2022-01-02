e32c2e7a83bbde7396280cd897e5bb0cf599e7cb
Abd-ElRahman007 <abdelrhman123work@gmail.com>
2022-01-02 03:52:49 +0200

    add the products route to the server file

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import users_routes from './handlers/users';
+import products_routes from './handlers/enchanted_products';
 
 const app: express.Application = express();
 const port: number = 8080;
@@ -13,6 +14,7 @@ const corsOptions = {
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 users_routes(app);
+products_routes(app);
 app.get('/', (req: Request, res: Response): void => {
   res.json('hello world');
 });
