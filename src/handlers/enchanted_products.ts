3b675b41c5ff16d7a672e415583d1e055f7e6f0c
Abd-ElRahman007 <abdelrhman123work@gmail.com>
2022-01-02 03:51:44 +0200

    add the rest sql models and the handlers for products

---
+++ src/handlers/enchanted_products.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from 'express';
+import { product, ProductsStore } from '../models/enchanted_products';
+import jwt from 'jsonwebtoken';
+
+const store: ProductsStore = new ProductsStore();
+
+const index = async (_req: Request, res: Response): Promise<void> => {
+  const products = await store.index();
+  res.json(products);
+};
+
+const show = async (req: Request, res: Response): Promise<void> => {
+  const product = await store.show((req.body.name as unknown) as string);
+  res.json(product);
+};
+const create = async (req: Request, res: Response): Promise<void> => {
+  const product = {
+    name: req.body.name as string,
+    price: req.body.price as number,
+    token: req.body.token as string
+  };
+  if (product.token) {
+    var decoded = jwt.verify(product.token, process.env.JWT_SECRET as string);
+    console.log(`verified`);
+  } else {
+    res.status(401);
+    throw new Error('No token');
+  }
+  try {
+    const newProduct = await store.create(product);
+    res.json(newProduct);
+  } catch (error) {
+    res.status(400);
+    res.json(`${error}`);
+  }
+}
+const products_routes = (app: express.Application) => {
+  app.get('/products', index);
+  app.post('/products/create', create);
+  app.get('/products/show', show);
+};
+export default products_routes;
--- src/models/enchanted_products.ts
+++ src/models/enchanted_products.ts
@@ -19,5 +19,36 @@ export class ProductsStore {
       throw new Error(`cannot get products ${error}`);
     }
   }
-  
+  async show(name: string): Promise<product> {
+    if (!name) {
+      throw new Error(`cannot find product with empty name`);
+    }
+    try {
+      const sql = 'SELECT * FROM enchanted_stuff WHERE name = $1;';
+      const conn: PoolClient = await client.connect();
+      const result = await conn.query(sql, [name]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`cannot find product name ${name}, error:${error}`);
+    }
+  }
+  async create(product: product): Promise<product> {
+    if (!product.name || !product.price) {
+      throw new Error(`cannot add new product and price with empty fields`);
+    }
+    if (await this.show(product.name)) {
+      throw new Error(`cannot add new product with name ${product.name}, it already exists`);
+    }
+    try {
+      const sql = 'INSERT INTO enchanted_stuff (name,price) VALUES ($1,$2) RETURNING *;';
+      const conn: PoolClient = await client.connect();
+      const result = await conn.query(sql, [product.name, product.price]);
+      const books = result.rows[0];
+      conn.release();
+      return books;
+    } catch (error) {
+      throw new Error(`cannot add new product title ${product.name}. error: ${error}`);
+    }
+  }
 }
