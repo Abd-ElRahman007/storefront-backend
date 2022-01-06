35c5c749892540674228f17c12c62d157cc9fd05
Abd-ElRahman007 <abdelrhman123work@gmail.com>
2022-01-06 15:32:12 +0200

    added the create and show for productsOrders table

--- src/service/dashboard.ts
+++ src/service/dashboard.ts
@@ -1,6 +1,72 @@
+import { PoolClient } from "pg";
 import client from "../database";
 
+
+export type productOrders = {
+  product_id: number;
+  order_id: number;
+  quantity: number;
+}
 export class DashboardQueries {
+  async create(o: productOrders): Promise<productOrders> {
+    if (!o.product_id || !o.order_id || !o.quantity) {
+      throw new Error('product_id, order_id and quantity are required');
+    }
+    try {
+      const productOrder = {
+        product_id: o.product_id,
+        order_id: o.order_id,
+        quantity: o.quantity
+      };
+      const sql = 'INSERT INTO product_orders (product_id,order_id,quantity) VALUES ($1,$2,$3) RETURNING *';
+      const conn: PoolClient = await client.connect();
+      const result = await conn.query(sql, [productOrder.product_id, productOrder.order_id, productOrder.quantity]);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`cannot add new productOrder ${o.product_id}. error: ${error}`);
+    }
+  }
+  async showOrders(id: productOrders): Promise<productOrders> {
+    if (!id) {
+      throw new Error('id is required');
+    }
+    try {
+      const conn = await client.connect();
+      const sql = `select * from product_orders where id=${id}`;
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`cannot get product_orders with id: ${error}`);
+    }
+  }
+  async indexOrder(): Promise<productOrders[]>{
+    try {
+      const sql = `select * from product_orders`;
+      const conn: PoolClient = await client.connect();
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (error) {
+      throw new Error(`cannot get orders: ${error}`);
+    }
+  }
+  async deleteOrder(id: number): Promise<productOrders|string> {
+    if (!id) {
+      throw new Error('id is required');
+    }
+    try {
+      const sql = `delete from product_orders where id=${id}`;
+      const conn: PoolClient = await client.connect();
+      const result = await conn.query(sql);
+      conn.release();
+      return `deleted`;
+    } catch (error) {
+      throw new Error(`cannot delete product_orders with id: ${error}`);
+    }
+  }
+
   async productsInOrder(): Promise<{ name: string, quantity: number, order_id: string }[]> {
     try {
       const conn = await client.connect();
--- src/service/handleDashboard.ts
+++ src/service/handleDashboard.ts
@@ -1,18 +1,91 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { DashboardQueries } from "./dashboard";
 
 const dashboard = new DashboardQueries();
 
 const productsInOrder = async (req: Request, res: Response): Promise<void> => {
-  const products = await dashboard.productsInOrder();
-  res.json(products);
+  try {
+    const products = await dashboard.productsInOrder();
+    res.json(products);
+  } catch (error) {
+    res.status(404)
+    res.json(`${error}`);
+  }
 }
 const usersWithOrders = async (req: Request, res: Response): Promise<void> => {
-  const users = await dashboard.usersWithOrders();
-  res.json(users);
+  try {
+    const users = await dashboard.usersWithOrders();
+    res.json(users);
+  } catch (error) {
+    res.status(404)
+    res.json(`${error}`);
+  }
 }
+const createOrder = async (req: Request, res: Response): Promise<void> => {
+  if (await dashboard.showOrders(req.body.order_id)) {
+    res.status(409);
+    throw new Error('Order already exists');
+  }
+  try {
+    const { quantity, order_id, product_id } = req.body;
+    const order = await dashboard.create({ quantity, order_id, product_id });
+    res.json(order);
+  } catch (error) {
+    res.status(404)
+    res.json(`${error}`);
+  }
+}
+
+const showOrders = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const order = await dashboard.showOrders(req.body.id);
+    res.json(order);
+  } catch (error) {
+    res.status(404)
+    res.json(`${error}`);
+  }
+}
+
+const indexOrder = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const orders = await dashboard.indexOrder();
+    res.json(orders);
+  } catch (error) {
+    res.status(404)
+    res.json(`${error}`);
+  }
+}
+
+const deleteOrder = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const order = await dashboard.deleteOrder(req.body.id);
+    res.json(order);
+  } catch (error) {
+    res.status(404)
+    res.json(`${error}`);
+  }
+}
+
+
+const verifyToken = (req: Request, res: Response, next: any): void => {
+  try {
+    const auth = req.headers.authorization as string;
+    const token = auth.split(' ')[1];
+    const result = jwt.verify(token, process.env.JWT_SECRET as string);
+    next();
+  } catch (error) {
+    res.status(401);
+    res.json(`${error}`);
+  }
+}
+
 const dashboardRoute = (app: express.Application): void => {
   app.get('/products-in-orders', productsInOrder);
   app.get('/users-with-orders', usersWithOrders);
+  app.post('/create-order', verifyToken, createOrder);
+  app.post('/show-orders', verifyToken, showOrders);
+  app.get('/index-orders', indexOrder);
+  app.delete('/delete-order', verifyToken, deleteOrder);
 }
 export default dashboardRoute;
